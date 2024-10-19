41e78fb8382cfe659c0ae8f8190770d4958869ff
andrewankenobi <andrea.vasco@outlook.com>
2024-10-19 06:54:23 +0100

    first model

--- sql_queries.sql
+++ sql_queries.sql
@@ -157,4 +157,100 @@ GROUP BY
   st.team_name
 ORDER BY
   avg_shots_on_target_percentage DESC
-    
+
+
+  -- average xg by team
+SELECT team.name AS team_name, AVG(shot.statsbomb_xg) AS average_xg
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE type.name = 'Shot'
+and  shot.statsbomb_xg IS NOT NULL
+GROUP BY team_name
+ORDER BY average_xg DESC    
+
+
+--shots under pressure by player
+SELECT player.name AS player_name, COUNT(*) AS shots_under_pressure
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE shot.statsbomb_xg IS NOT NULL AND under_pressure = TRUE
+GROUP BY player_name
+ORDER BY shots_under_pressure DESC;
+
+
+-- recoveries by player
+SELECT player.name AS player_name, COUNT(*) AS recoveries
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE type.name = "Ball Recovery"
+GROUP BY player_name
+ORDER BY recoveries DESC
+
+
+-- matches with score
+SELECT match_id, home_team.home_team_name, away_team.away_team_name, home_score, away_score
+FROM `awesome-advice-420021.statsbomb.matches`
+ORDER BY match_id;
+
+-- goals by team and play pattern
+SELECT team.name, play_pattern.name AS play_pattern, COUNT(*) AS goals
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE shot.outcome.name = "Goal"
+GROUP BY team.name, play_pattern
+order by 1,2
+
+-- substitutes by player 
+SELECT player.name AS player_name, COUNT(*) AS appearances
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE type.name = "Substitution"
+GROUP BY player_name
+ORDER BY appearances DESC
+
+-- player with most appearances
+SELECT player.name AS player_name, COUNT(*) AS appearances
+FROM `awesome-advice-420021.statsbomb.events`
+WHERE type.name = "Substitution"
+GROUP BY player_name
+ORDER BY appearances DESC
+
+
+-- player clusters
+CREATE OR REPLACE MODEL `awesome-advice-420021.statsbomb.player_clusters`
+OPTIONS(model_type='kmeans', num_clusters=5) AS
+SELECT
+  player.name,
+  COUNT(CASE WHEN type.name = 'Ball Recovery' THEN 1 END) AS Ball_Recovery_count,
+  COUNT(CASE WHEN type.name = 'Dispossessed' THEN 1 END) AS Dispossessed_count,
+  COUNT(CASE WHEN type.name = 'Duel' THEN 1 END) AS Duel_count,
+  COUNT(CASE WHEN type.name = 'Block' THEN 1 END) AS Block_count,
+  COUNT(CASE WHEN type.name = 'Offside' THEN 1 END) AS Offside_count,
+  COUNT(CASE WHEN type.name = 'Clearance' THEN 1 END) AS Clearance_count,
+  COUNT(CASE WHEN type.name = 'Interception' THEN 1 END) AS Interception_count,
+  COUNT(CASE WHEN type.name = 'Dribble' THEN 1 END) AS Dribble_count,
+  COUNT(CASE WHEN type.name = 'Shot' THEN 1 END) AS Shot_count,
+  COUNT(CASE WHEN type.name = 'Pressure' THEN 1 END) AS Pressure_count,
+  COUNT(CASE WHEN type.name = 'Half Start' THEN 1 END) AS Half_Start_count,
+  COUNT(CASE WHEN type.name = 'Substitution' THEN 1 END) AS Substitution_count,
+  COUNT(CASE WHEN type.name = 'Own Goal Against' THEN 1 END) AS Own_Goal_Against_count,
+  COUNT(CASE WHEN type.name = 'Foul Won' THEN 1 END) AS Foul_Won_count,
+  COUNT(CASE WHEN type.name = 'Foul Committed' THEN 1 END) AS Foul_Committed_count,
+  COUNT(CASE WHEN type.name = 'Goal Keeper' THEN 1 END) AS Goal_Keeper_count,
+  COUNT(CASE WHEN type.name = 'Bad Behaviour' THEN 1 END) AS Bad_Behaviour_count,
+  COUNT(CASE WHEN type.name = 'Own Goal For' THEN 1 END) AS Own_Goal_For_count,
+  COUNT(CASE WHEN type.name = 'Player On' THEN 1 END) AS Player_On_count,
+  COUNT(CASE WHEN type.name = 'Player Off' THEN 1 END) AS Player_Off_count,
+  COUNT(CASE WHEN type.name = 'Shield' THEN 1 END) AS Shield_count,
+  COUNT(CASE WHEN type.name = 'Pass' THEN 1 END) AS Pass_count,
+  COUNT(CASE WHEN type.name = '50/50' THEN 1 END) AS Fifty_Fifty_count,
+  COUNT(CASE WHEN type.name = 'Half End' THEN 1 END) AS Half_End_count,
+  COUNT(CASE WHEN type.name = 'Starting XI' THEN 1 END) AS Starting_XI_count,
+  COUNT(CASE WHEN type.name = 'Tactical Shift' THEN 1 END) AS Tactical_Shift_count,
+  COUNT(CASE WHEN type.name = 'Error' THEN 1 END) AS Error_count,
+  COUNT(CASE WHEN type.name = 'Miscontrol' THEN 1 END) AS Miscontrol_count,
+  COUNT(CASE WHEN type.name = 'Dribbled Past' THEN 1 END) AS Dribbled_Past_count,
+  COUNT(CASE WHEN type.name = 'Injury Stoppage' THEN 1 END) AS Injury_Stoppage_count,
+  COUNT(CASE WHEN type.name = 'Referee Ball-Drop' THEN 1 END) AS Referee_Ball_Drop_count,
+  COUNT(CASE WHEN type.name = 'Ball Receipt*' THEN 1 END) AS Ball_Receipt_count,
+  COUNT(CASE WHEN type.name = 'Carry' THEN 1 END) AS Carry_count,
+  COUNT(*) AS total_events
+FROM
+  `awesome-advice-420021.statsbomb.events`
+GROUP BY
+  player.name
