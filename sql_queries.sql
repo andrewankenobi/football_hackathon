e8ae77551a58aa91a50c5567a2a3c702c877fc54
andrewankenobi <andrea.vasco@outlook.com>
2024-10-19 08:15:37 +0100

    Match Outcome Prediction

--- sql_queries.sql
+++ sql_queries.sql
@@ -254,3 +254,185 @@ FROM
   `awesome-advice-420021.statsbomb.events`
 GROUP BY
   player.name
+
+
+-- Model to predict xg from shot type, body part, technique, under pressure, play pattern, location
+
+CREATE OR REPLACE MODEL
+  `awesome-advice-420021.statsbomb.xg_prediction` OPTIONS(model_type='linear_reg',
+    input_label_cols=['statsbomb_xg']) AS
+SELECT
+  shot.type.name AS shot_type,
+  shot.body_part.name AS body_part,
+  shot.technique.name AS technique,
+  under_pressure,
+  play_pattern.name,
+  location,
+  shot.statsbomb_xg
+FROM
+  `awesome-advice-420021.statsbomb.events`
+WHERE
+  shot.statsbomb_xg IS NOT NULL;
+
+
+
+-- Model to predict if a shot is a goal or not
+CREATE OR REPLACE MODEL `awesome-advice-420021.statsbomb.goal_prediction_model`
+OPTIONS(
+  model_type='LOGISTIC_REG',
+  input_label_cols=['is_goal']
+) AS
+SELECT
+  CASE WHEN shot.outcome.name = 'Goal' THEN 1 ELSE 0 END as is_goal,
+  shot.body_part.name AS body_part,
+  shot.technique.name AS technique,
+  shot.type.name AS shot_type,
+  under_pressure,
+  play_pattern.name AS play_pattern,
+  player.name,
+  period,
+  SQRT(POW(120 - location[OFFSET(0)], 2) + POW(40 - location[OFFSET(1)], 2)) AS distance_to_goal,
+  ABS(ATAN2(40 - location[OFFSET(1)], 120 - location[OFFSET(0)])) AS angle_to_goal,
+  CASE 
+    WHEN location[OFFSET(0)] >= 102 AND location[OFFSET(1)] BETWEEN 18 AND 62 THEN 1 
+    ELSE 0 
+  END AS is_in_box,
+  CASE 
+    WHEN ABS(location[OFFSET(1)] - 40) < 20 THEN 1 
+    ELSE 0 
+  END AS is_central,
+  CASE 
+    WHEN minute >= 75 THEN 1 
+    ELSE 0 
+  END AS is_late_game
+FROM
+  `awesome-advice-420021.statsbomb.events`
+WHERE
+  type.name = 'Shot'
+  AND shot.type.name != 'Penalty'
+  AND player.name IS NOT NULL
+
+
+-- Testing the model with explanation
+
+SELECT
+  *
+FROM
+  ML.EXPLAIN_PREDICT(MODEL `awesome-advice-420021.statsbomb.goal_prediction_model`,
+    (
+        SELECT
+   CASE WHEN shot.outcome.name = 'Goal' THEN 1 ELSE 0 END as is_goal_real,
+  shot.body_part.name AS body_part,
+  shot.technique.name AS technique,
+  shot.type.name AS shot_type,
+  under_pressure,
+  play_pattern.name AS play_pattern,
+  player.name,
+  period,
+  SQRT(POW(120 - location[OFFSET(0)], 2) + POW(40 - location[OFFSET(1)], 2)) AS distance_to_goal,
+  ABS(ATAN2(40 - location[OFFSET(1)], 120 - location[OFFSET(0)])) AS angle_to_goal,
+  CASE 
+    WHEN location[OFFSET(0)] >= 102 AND location[OFFSET(1)] BETWEEN 18 AND 62 THEN 1 
+    ELSE 0 
+  END AS is_in_box,
+  CASE 
+    WHEN ABS(location[OFFSET(1)] - 40) < 20 THEN 1 
+    ELSE 0 
+  END AS is_central,
+  CASE 
+    WHEN minute >= 75 THEN 1 
+    ELSE 0 
+  END AS is_late_game
+FROM
+  `awesome-advice-420021.statsbomb.events`
+WHERE
+  type.name = 'Shot'
+  AND shot.type.name != 'Penalty'
+  AND player.name IS NOT NULL
+
+    )
+  )
+
+
+-- Create view with some stats per match
+
+CREATE OR REPLACE VIEW `awesome-advice-420021.statsbomb.match_stats` as (
+
+WITH match_stats AS (
+  SELECT
+    e.match_id,
+    e.period,
+    e.minute,
+    e.second,
+    m.home_team.home_team_name AS home_team,
+    m.away_team.away_team_name AS away_team,
+    m.home_score,
+    m.away_score,
+    SUM(CASE WHEN e.team.name = m.home_team.home_team_name THEN e.duration ELSE 0 END) AS home_possession,
+    SUM(CASE WHEN e.team.name = m.away_team.away_team_name THEN e.duration ELSE 0 END) AS away_possession,
+    SUM(CASE WHEN e.type.name = 'Shot' AND e.team.name = m.home_team.home_team_name THEN 1 ELSE 0 END) AS home_shots,
+    SUM(CASE WHEN e.type.name = 'Shot' AND e.team.name = m.away_team.away_team_name THEN 1 ELSE 0 END) AS away_shots,
+    SUM(CASE WHEN e.type.name = 'Shot' AND e.shot.outcome.name = 'Goal' AND e.team.name = m.home_team.home_team_name THEN 1 ELSE 0 END) AS home_goals,
+    SUM(CASE WHEN e.type.name = 'Shot' AND e.shot.outcome.name = 'Goal' AND e.team.name = m.away_team.away_team_name THEN 1 ELSE 0 END) AS away_goals,
+    SUM(CASE WHEN e.type.name = 'Pass' AND e.team.name = m.home_team.home_team_name THEN 1 ELSE 0 END) AS home_passes,
+    SUM(CASE WHEN e.type.name = 'Pass' AND e.team.name = m.away_team.away_team_name THEN 1 ELSE 0 END) AS away_passes,
+    SUM(CASE WHEN e.type.name = 'Foul Committed' AND e.team.name = m.home_team.home_team_name THEN 1 ELSE 0 END) AS home_fouls,
+    SUM(CASE WHEN e.type.name = 'Foul Committed' AND e.team.name = m.away_team.away_team_name THEN 1 ELSE 0 END) AS away_fouls,
+    SUM(CASE WHEN e.type.name = 'Duel' AND e.team.name = m.home_team.home_team_name THEN 1 ELSE 0 END) AS home_duels,
+    SUM(CASE WHEN e.type.name = 'Duel' AND e.team.name = m.away_team.away_team_name THEN 1 ELSE 0 END) AS away_duels,
+    COALESCE(AVG(CASE WHEN e.type.name = 'Shot' AND e.team.name = m.home_team.home_team_name THEN e.shot.statsbomb_xg END), 0) AS home_avg_xg,
+    COALESCE(AVG(CASE WHEN e.type.name = 'Shot' AND e.team.name = m.away_team.away_team_name THEN e.shot.statsbomb_xg END), 0) AS away_avg_xg
+  FROM
+    `awesome-advice-420021.statsbomb.events` e
+  JOIN
+    `awesome-advice-420021.statsbomb.matches` m ON e.match_id = m.match_id
+  GROUP BY
+    e.match_id, e.period, e.minute, e.second, m.home_team.home_team_name, m.away_team.away_team_name, m.home_score, m.away_score
+)
+
+SELECT
+  CASE
+    WHEN home_score > away_score THEN 'home_win'
+    WHEN home_score < away_score THEN 'away_win'
+    ELSE 'draw'
+  END AS match_outcome,
+  period,
+  minute,
+  second,
+  home_team,
+  away_team,
+  home_score - away_score AS score_difference,
+  CASE WHEN (home_possession + away_possession) > 0 THEN home_possession / (home_possession + away_possession) ELSE 0 END AS home_possession_percentage,
+  home_shots,
+  away_shots,
+  home_goals,
+  away_goals,
+  home_passes,
+  away_passes,
+  home_fouls,
+  away_fouls,
+  home_duels,
+  away_duels,
+  home_avg_xg,
+  away_avg_xg,
+  CASE WHEN minute > 0 THEN (home_shots - away_shots) / minute ELSE 0 END AS shot_difference_per_minute,
+  CASE WHEN minute > 0 THEN (home_passes - away_passes) / minute ELSE 0 END AS pass_difference_per_minute,
+  CASE WHEN minute > 0 THEN (home_fouls - away_fouls) / minute ELSE 0 END AS foul_difference_per_minute,
+  CASE WHEN minute > 0 THEN (home_duels - away_duels) / minute ELSE 0 END AS duel_difference_per_minute,
+  home_avg_xg - away_avg_xg AS xg_difference
+FROM
+  match_stats
+WHERE
+  minute > 0
+
+  )
+
+--  Model to predict match outcome
+CREATE OR REPLACE MODEL `awesome-advice-420021.statsbomb.match_outcome_prediction`
+OPTIONS(
+  model_type='RANDOM_FOREST_CLASSIFIER',
+  num_trials=20,
+  input_label_cols=['match_outcome']
+) AS
+
+select * from `awesome-advice-420021.statsbomb.match_stats`
